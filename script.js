a01362c3d64c5808eea213942fbc0acf15bf704a
dev-sahil-bharti <sahil.884062@gmail.com>
2025-08-19 20:33:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typedef = new Typed(".multipul-text", {
+    strings: ["Frontend Developer", "Backend Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
